2e165ad9e887b8e9c8e8fa519d5cfd9e173a050a
sao17u328 <alex@MBP-Aleksandr.Dlink>
2018-03-24 23:55:40 +0300

    num3 was complite

---
+++ lab_02_3_3/lab_02_3_3.c
@@ -0,0 +1,33 @@
+/*
+Программа вычисляет ряд arctg и подсчитывает ошибку.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(void)
+{
+    double x, eps;
+    printf("x: ");
+    scanf("%lf", &x);
+    printf("eps: ");
+    scanf("%lf", &eps);
+
+    double t1 = 0, t2 = eps, sum = 0;
+    int a = 1, k = 1;
+    while (fabs(t1 - t2) >= eps)
+    {
+        t2 = t1;
+        t1 = x / a;
+        sum += k * t1;
+        x *= x * x;
+        k *= -1;
+        a += 2;
+    }
+
+    double arc = atan(x);
+    printf("Sum = %-8.5lf\nOtv = %-8.5lf\nAbsolute = %-8.5lf\nRelative = %-8.5lf\n",
+           sum, arc, fabs(sum - arc), fabs((sum - arc) / arc));
+    return 0;
+}
