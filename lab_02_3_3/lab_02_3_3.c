dfb5cc9250cda4f4b3d780d309c5215e97a252c3
Stepanov Alexandr <happymainer@gmail.com>
2018-03-25 15:13:41 +0300

    Update num3

--- lab_02_3_3/lab_02_3_3.c
+++ lab_02_3_3/lab_02_3_3.c
@@ -6,14 +6,8 @@
 #include<stdlib.h>
 #include<math.h>
 
-int main(void)
+double f(double x, double eps)
 {
-    double x, eps;
-    printf("x: ");
-    scanf("%lf", &x);
-    printf("eps: ");
-    scanf("%lf", &eps);
-
     double t1 = 0, t2 = eps, sum = 0;
     int a = 1, k = 1;
     while (fabs(t1 - t2) >= eps)
@@ -25,6 +19,18 @@ int main(void)
         k *= -1;
         a += 2;
     }
+    return sum;
+}
+
+int main(void)
+{
+    double x, eps;
+    printf("x: ");
+    scanf("%lf", &x);
+    printf("eps: ");
+    scanf("%lf", &eps);
+
+    sum = f(x, eps);
 
     double arc = atan(x);
     printf("Sum = %-8.5lf\nOtv = %-8.5lf\nAbsolute = %-8.5lf\nRelative = %-8.5lf\n",
