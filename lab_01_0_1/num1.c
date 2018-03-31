699bb26285566e17709f1f20e0948a2ead707a86
Stepanov Alexandr <happymainer@gmail.com>
2018-03-31 14:40:25 +0300

    Update num1.c

--- lab_01_0_1/num1.c
+++ lab_01_0_1/num1.c
@@ -1,4 +1,6 @@
-// Даны основания и высота равнобедренной трапеции. Найти периметр трапеции.
+/* 
+ * Даны основания и высота равнобедренной трапеции. Найти периметр трапеции.
+*/
 
 #include<stdio.h>
 #include<math.h>
@@ -8,18 +10,24 @@
 int main(void)
 {
   float a, b, h;
-
-  printf("Введите длины оснований через пробел: ");
-  scanf("%f %f", &a, &b);
-
+  printf("Введите основания: ");
+  if (scanf("%f%f", &a, &b) != 2)
+  {
+    printf("Некорректный ввод\n");
+    return 0;
+  }
   printf("Введите высоту: ");
-  scanf("%f", &h);
+  if (scanf("%f", &h) != 1)
+  {
+    printf("Некорректный ввод\n");
+    return 0;
+  }
 
   if (a > 0 && b > 0 && h > 0)
   {
-    float p;
-    p = a + b + TWO * sqrt(h * h + fabsf(a - b) * fabsf(a - b));
-    printf("Периметр: %f\n", p);
+    float per;
+    per = a + b + TWO * sqrt(h * h + fabsf(a - b) * fabsf(a - b));
+    printf("Периметр: %8.3f\n", per);
   }
   else
   {
