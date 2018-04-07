a3c29bd235b285073442c64f044714f6ab09ab45
Stepanov Alexandr <happymainer@gmail.com>
2018-04-07 14:26:09 +0300

    Update lab_02_3_2.c

--- lab_02_3_2/lab_02_3_2.c
+++ lab_02_3_2/lab_02_3_2.c
@@ -4,6 +4,7 @@
 */
 #include<stdio.h>
 
+#define SUCCESS 0
 #define INPUT_ERROR 1
 
 //Функция для нахождения положения точки
@@ -36,27 +37,23 @@ void where(int x1, int y1, int x2, int y2, int x, int y)
 int main(void)
 {
     int x1, x2, y1, y2, x, y;
-
     printf("Первая точка прямой: ");
     if (scanf("%d%d", &x1, &y1) != 2)
     {
         printf("Некорректный ввод!\n");
         return INPUT_ERROR;
     }
-
     printf("Вторая точка прямой: ");
     if (scanf("%d%d", &x2, &y2) != 2)
     {
         printf("Некорректный ввод!\n");
         return INPUT_ERROR;
     }
-
     if (x1 == x2 && y1 == y2)
     {
         printf("Введены две одинаковые точки\n");
         return INPUT_ERROR
     }
-
     printf("Точка: ");
     if (scanf("%d%d", &x, &y) != 2)
     {
@@ -65,5 +62,5 @@ int main(void)
     }
 
     where(x1, y1, x2, y2, x, y);
-    return 0;
+    return SUCCESS;
 }
