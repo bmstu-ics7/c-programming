53d1c7a85400031cb421c045ac792d0064e50c8a
U-VLAB\user-lab01 <user-lab01@LAB-01-11.vlab.bmstu.ru>
2018-04-14 14:26:23 +0300

    First task complete

---
+++ RK_21_5_1/rk_21_5_1.c
@@ -0,0 +1,42 @@
+/* Задача 1. Вариант 5.
+ * Дано натуральное число. 
+ * Установить, является ли последовательность его цифр при просмотре
+ * их слева направо упорядоченной по возрастанию. 
+*/
+#include <stdio.h>
+
+#define N 10
+#define SUCCESS 0
+#define INPUT_ERROR -1
+#define TEN 10
+
+int main(void)
+{
+	
+  	int num;
+	if (scanf("%d", &num) != 1)
+  	{
+    		printf("Некорректный ввод!");
+    		return INPUT_ERROR;
+  	}
+  
+  	int arr[N];
+	int n = 0;
+  	while (num)
+  	{
+		arr[n] = num % TEN;
+		num /= TEN;
+		n++;
+  	}
+	
+	int flag = 1;
+	for (int i = 0; i < n - 1; i++)
+		if (arr[i] <= arr[i + 1])
+		{
+			flag = 0;
+			break;
+		}
+
+	printf("%d", flag);
+  	return SUCCESS;
+}
