0d3a4ad360a6f43fd57a5682d1a2f9a77d800420
sao17u328 <alex@MacBook-Pro-Aleksandr.local>
2018-04-16 09:12:28 +0300

    Second task complete

---
+++ lab_03_2_1/lab_03_2_1.c
@@ -0,0 +1,106 @@
+/*
+ * Найти количество чисел, значение которых больше среднего арифметического
+ * минимального и максимального чисел.
+*/
+#include <stdio.h>
+
+#define SUCCESS 0
+#define ARG_ERROR -1
+#define VOID_FILE -2
+#define INCORECT_FILE -3
+
+#define ZERO 0
+#define ONE 1
+#define TWO 2
+
+#define TRUE 1
+#define FALSE 0
+
+int find_average(FILE* file, float* average)
+{
+    float num;
+    float max = ZERO, min = ZERO;
+    int first_iteration = TRUE;
+
+    while(fscanf(file, "%f", &num) == ONE)
+    {
+        if (first_iteration)
+        {
+            max = num;
+            min = num;
+            first_iteration = FALSE;
+        }
+
+        if (max < num)
+            max = num;
+        if (min > num)
+            min = num;
+    }
+
+    if (first_iteration && feof(file))
+        return VOID_FILE;
+
+    if (!feof(file))
+        return INCORECT_FILE;
+
+    *average = (max + min) / 2;
+    return SUCCESS;
+}
+
+int find_count(FILE* file, const float average, int* count)
+{
+    float num;
+    *count = ZERO;
+
+    while(fscanf(file, "%f", &num) == 1)
+    {
+        if (num > average)
+            (*count)++;
+    }
+
+    return SUCCESS;
+}
+
+int write_file(FILE* file, const int count)
+{
+    fprintf(file, "%d", count);
+    return SUCCESS;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != TWO)
+    {
+        printf("Некоректное количество входных данных\n");
+        return ARG_ERROR;
+    }
+
+    float average;
+    int count;
+
+    FILE* file;
+    file = fopen(argv[ONE], "r");
+
+    switch (find_average(file, &average))
+    {
+        case VOID_FILE:
+            printf("Файл пустой!");
+            return VOID_FILE;
+
+        case INCORECT_FILE:
+            printf("Файл некорректный!");
+            return INCORECT_FILE;
+    }
+
+    fclose(file);
+
+    file = fopen(argv[ONE], "r");
+    find_count(file, average, &count);
+    fclose(file);
+
+    file = fopen("out.txt", "w");
+    write_file(file, count);
+    fclose(file);
+
+    return SUCCESS;
+}
