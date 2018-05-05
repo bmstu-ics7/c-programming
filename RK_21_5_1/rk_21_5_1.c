83d13481914480fc36f5928455579dbb0b41a5a4
Stepanov Alexandr <happymainer@gmail.com>
2018-05-05 16:47:31 +0300

    Codestyle fixed

--- RK_21_5_1/rk_21_5_1.c
+++ RK_21_5_1/rk_21_5_1.c
@@ -14,31 +14,33 @@
 
 int main(void)
 {
-  	int num;
-	printf("Введите число: ");
-	if (scanf("%d", &num) != 1)
-  	{
-    		printf("Некорректный ввод!");
-    		return INPUT_ERROR;
-  	}
-  
-  	int arr[N];
-	int n = 0;
-  	while (num)
-  	{
-		arr[n] = num % TEN;
-		num /= TEN;
-		n++;
-  	}
-	
-	int flag = TRUE;
-	for (int i = 0; i < n - 1; i++)
-		if (arr[i] <= arr[i + 1])
-		{
-			flag = FALSE;
-			break;
-		}
+    int num;
 
-	printf("%d", flag);
-  	return SUCCESS;
+    printf("Введите число: ");
+    if (scanf("%d", &num) != 1)
+    {
+        printf("Некорректный ввод!");
+    	return INPUT_ERROR;
+    }
+  
+    int arr[N];
+    int n = 0;
+
+    while (num)
+    {
+	arr[n] = num % TEN;
+	num /= TEN;
+	n++;
+    }
+	
+    int flag = TRUE;
+    for (int i = 0; i < n - 1; i++)
+	if (arr[i] <= arr[i + 1])
+	{
+	    flag = FALSE;
+	    break;
+	}
+
+    printf("%d", flag);
+    return SUCCESS;
 }
