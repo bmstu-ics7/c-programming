11dda476009d991d0efd72178fa5a03b3f604725
Stepanov Alexandr <happymainer@gmail.com>
2018-05-12 20:32:08 +0300

    Tests update

--- lab_06_1_3/test_calc.c
+++ lab_06_1_3/test_calc.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include "calc.h"
 
+#define SUCCESS 0
+#define WRONG -1
+
+int test1(void)
+{
+    int a[] = {1, 2, -1};
+    int k = calc(a, a + 2);
+
+    if (k == -2)
+    {
+        printf("Test {1, 2, -1} true answer: -2\n");
+        return SUCCESS;
+    }
+    else
+    {
+        printf("Test {1, 2, -1} wrong answer: %d. True: -2\n", k);
+        return WRONG;
+    }
+}
+
+int test2(void)
+{
+    int a[] = {3, 5, -2, -4};
+    int k = calc(a, a + 3);
+
+    if (k == -22)
+    {
+        printf("Test {3, 5, -2, -4} true answer: -22\n");
+        return SUCCESS;
+    }
+    else
+    {
+        printf("Test {3, 5, -2, -4} wrong answer: %d. True: -22\n", k);
+        return WRONG;
+    }
+}
+
 void test_calc(void)
 {
     int err_cnt = 0;
 
-    {
-        int a[] = {1, 2, -1};
-        if (calc(a, a + 2) != -2)
-            err_cnt++;
+    if (test1() != SUCCESS)
+        err_cnt++;
 
-        int b[] = {3, 5, -2, -4};
-        if (calc(b, b + 3) != -22)
-            err_cnt++;
-    }
+    if (test2() != SUCCESS)
+        err_cnt++;
 
     printf("%d %s\n", err_cnt, err_cnt ? "FAILED" : "OK");
 }
--- lab_06_1_3/test_input_array.c
+++ lab_06_1_3/test_input_array.c
@@ -5,41 +5,77 @@
 #define VOID_FILE -3
 #define INCORRECT_FILE -4
 
-//int input_array(FILE* file, int* array, int* size);
+#define WRONG -5
+
+int test1(void)
+{
+    FILE* f = fopen("1.in", "r");
+    int a[100], size = 0;
+
+    if (input_array(f, a, &size) == VOID_FILE)
+    {
+        fclose(f);
+        printf("File 1.in true return: VOID_FILE.\n");
+        return SUCCESS;
+    }
+    else
+    {
+        fclose(f);
+        printf("File 1.in wrong return. True return: VOID_FILE.");
+        return WRONG;
+    }
+}
+
+int test2(void)
+{
+    FILE* f = fopen("2.in", "r");
+    int a[100], size = 0;
+
+    if (input_array(f, a, &size) == INCORRECT_FILE)
+    {
+        fclose(f);
+        printf("File 2.in true return: INCORRECT_FILE.\n");
+        return SUCCESS;
+    }
+    else
+    {
+        fclose(f);
+        printf("File 2.in wrong return. True return: INCORRECT_FILE.");
+        return WRONG;
+    }
+}
+
+int test3(void)
+{
+    FILE* f = fopen("3.in", "r");
+    int a[100], size = 0;
+
+    if (input_array(f, a, &size) == SUCCESS)
+    {
+        printf("File 3.in true return: SUCCESS.\n");
+        return SUCCESS;
+    }
+    else
+    {
+        printf("File 3.in wrong return. True return: SUCCESS.");
+        return WRONG;
+    }
+}
 
 void test_input_array(void)
 {
     int err_cnt = 0;
 
-    {
-        FILE* f = fopen("1.in", "r");
-        int a[100], size;
+    if (test1() != SUCCESS)
+        err_cnt++;
 
-        if (input_array(f, a, &size) != VOID_FILE)
-            err_cnt++;
+    if (test2() != SUCCESS)
+        err_cnt++;
 
-        fclose(f);
+    if (test3() != SUCCESS)
+        err_cnt++;
 
-        f = fopen("2.in", "r");
-        int b[100];
-        size = 0;
-
-        if (input_array(f, b, &size) != INCORRECT_FILE)
-            err_cnt++;
-
-        fclose(f);
-
-        f = fopen("3.in", "r");
-        int c[100];
-        size = 0;
-
-        if (input_array(f, c, &size) != SUCCESS)
-            err_cnt++;
-
-        fclose(f);
-    }
-
-    printf("%d %s", err_cnt, err_cnt ? "FAILED" : "OK");
+    printf("%d %s\n", err_cnt, err_cnt ? "FAILED" : "OK");
 }
 
 int main(void)
