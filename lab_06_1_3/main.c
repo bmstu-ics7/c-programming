113b73e8f88d3a3c072f2d0082c9a4d8097cfd17
Stepanov Alexandr <happymainer@gmail.com>
2018-05-12 18:43:42 +0300

    Third task complete

---
+++ lab_06_1_3/main.c
@@ -0,0 +1,48 @@
+/*
+ * main.c
+ */
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include "define.h"
+#include "input_array.h"
+#include "calc.h"
+#include "read_error.h"
+
+//int input_array(FILE* file, int* array, int* size);
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("Некорректное количество аргументов!");
+        return ARG_ERROR;
+    }
+
+    FILE* file = NULL;
+    file = fopen(argv[1], "r");
+
+    if (file == NULL)
+    {
+        printf("%s", strerror(errno));
+        return errno;
+    }
+
+    int array[N], count = 0;
+    int error = 0;
+
+    if ((error = input_array(file, array, &count) != SUCCESS))
+    {
+        return read_error(error);
+    }
+
+    if (fclose(file) != 0)
+    {
+        printf("%s", strerror(errno));
+        return errno;
+    }
+
+    printf("%d\n", calc(array, array + count - 1));
+    return SUCCESS;
+}
+
---
+++ lab_06_1_3/read_error.h
@@ -0,0 +1,6 @@
+#ifndef READ_ERROR_H
+#define READ_ERROR_H
+
+int read_error(int err);
+
+#endif
