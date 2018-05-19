00c37334419ae55c8c4a7225e0eed5ddcfb73d87
U-VLAB\user-lab01 <user-lab01@LAB-01-15.vlab.bmstu.ru>
2018-05-19 15:27:06 +0300

    Add test2 for deg_mat

--- rk_22_3/test_deg_mat.c
+++ rk_22_3/test_deg_mat.c
@@ -30,11 +30,40 @@ int test1(void)
     return SUCCESS;
 }
 
+int test2(void)
+{
+    int a[] = {1, 0, 0, 1};
+    int b[] = {0, 0, 0, 0};
+    int k[] = {0, 0, 0, 0};
+    int c[100], size = 0;
+
+    deg_mat(a, 4, b, 4, c, &size);
+
+    printf("test1\n");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d %d\n", k[i], c[i]);
+
+        if (c[i] != k[i])
+        {
+            printf("test1 failed\n\n");
+            return WRONG;
+        }
+    }
+
+    printf("\n");
+    return SUCCESS;
+}
+
 void test_deg_mat(void)
 {
     int err_cnt = 0;
 
     if (test1() != SUCCESS)
+        err_cnt++;
+
+    if (test2() != SUCCESS)
         err_cnt++;
 
     printf("%d %s", err_cnt, err_cnt ? "FAILED" : "OK");
