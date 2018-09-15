dfc66f52ebb1b9d8b13550ede2627c7c4159860a
Stepanov Alexandr <happymainer@gmail.com>
2018-09-15 17:06:12 +0300

    Add const in arguments in print_array()

--- lab_07_2/print_array.c
+++ lab_07_2/print_array.c
@@ -1,6 +1,6 @@
 #include "print_array.h"
 
-void print_array(FILE* file, const int* pa, const int* const pb)
+void print_array(FILE* file, const int* const pa, const int* const pb)
 {
     for (const int* p = pa; p < pb; p++)
         fprintf(file, "%d ", *p);
--- lab_07_2/print_array.h
+++ lab_07_2/print_array.h
@@ -3,6 +3,6 @@
 #ifndef PRINT_ARRAY_H
 #define PRINT_ARRAY_H
 
-void print_array(FILE* file, const int* pa, const int* const pb);
+void print_array(FILE* file, const int* const pa, const int* const pb);
 
 #endif
