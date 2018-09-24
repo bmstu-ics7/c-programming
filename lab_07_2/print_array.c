582a0d101c4b140b7fc1fdd8df49f6cc4b19fcfd
Stepanov Alexandr <happymainer@gmail.com>
2018-09-24 12:13:54 +0300

    Fix codestyle in print_array

--- lab_07_2/print_array.c
+++ lab_07_2/print_array.c
@@ -1,8 +1,8 @@
 #include "print_array.h"
 
-void print_array(FILE* file, const int* const pa, const int* const pb)
+void print_array(FILE* file, const int *const pa, const int *const pb)
 {
-    for (const int* p = pa; p < pb; p++)
+    for (const int *p = pa; p < pb; p++)
         fprintf(file, "%d ", *p);
 
     fprintf(file, "\n");
