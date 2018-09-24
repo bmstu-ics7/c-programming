eb0dd251974b416122a0e6fe35fe76f7f944fa9e
Stepanov Alexandr <happymainer@gmail.com>
2018-09-24 12:11:34 +0300

    Fix codestyle in mysort

--- lab_07_2/mysort.c
+++ lab_07_2/mysort.c
@@ -1,22 +1,22 @@
 #include "mysort.h"
 
 // По возрастанию
-int compare_inc(const void* p, const void* q)
+int compare_inc(const void *p, const void *q)
 {
     return *(int*)p - *(int*)q;
 }
 
 // По убыванию
-int compare_dec(const void* p, const void* q)
+int compare_dec(const void *p, const void *q)
 {
     return *(int*)q - *(int*)p;
 }
 
-void swap(void* to, void* from, size_t len)
+void swap(void *to, void *from, size_t len)
 {
     char tmp;
-    char* tmp_to = to;
-    char* tmp_from = from;
+    char *tmp_to = to;
+    char *tmp_from = from;
 
     for (size_t i = 0; i < len; i++)
     {
@@ -26,13 +26,12 @@ void swap(void* to, void* from, size_t len)
     }
 }
 
-void mysort(void *base, size_t nmemb, size_t size,
-	     int (*compar)(const void*, const void*))
+void mysort(void *base, size_t nmemb, size_t size, int (*compar)(const void*, const void*))
 {
     for (int i = 0; i < nmemb; i++)
     {
-        char* max = base;
-        for (char* j = base; j < (char*)base + (nmemb - i) * size; j += size)
+        char *max = base;
+        for (char *j = base; j < (char*)base + (nmemb - i) * size; j += size)
             if (compar(j, max) > 0)
             {
                 max = j;
