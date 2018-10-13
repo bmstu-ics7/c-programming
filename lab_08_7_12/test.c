c8f3b45616d7956bbdc36d5c7d93f51ccbfcfe46
Stepanov Alexandr <happymainer@gmail.com>
2018-10-13 09:57:42 +0300

    Fix codestyle in test.c

--- lab_08_7_12/test.c
+++ lab_08_7_12/test.c
@@ -17,11 +17,11 @@ void test1_sum(void)
 {
     double **right;
     right = allocate_matrix(3, 3);
-    double r_arr[] = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    double r_arr[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2 };
     generate_matrix(right, 3, 3, r_arr);
 
     double **a = allocate_matrix(3, 3);
-    double a_arr[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+    double a_arr[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
     generate_matrix(a, 3, 3, a_arr);
 
     double **b = allocate_matrix(3, 3);
@@ -42,15 +42,15 @@ void test2_sum(void)
 {
     double **right;
     right = allocate_matrix(3, 3);
-    double r_arr[] = {9, 9, 9, 9, 9, 9, 9, 9 ,9};
+    double r_arr[] = { 9, 9, 9, 9, 9, 9, 9, 9 ,9 };
     generate_matrix(right, 3, 3, r_arr);
 
     double **a = allocate_matrix(3, 3);
-    double a_arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    double a_arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     generate_matrix(a, 3, 3, a_arr);
 
     double **b = allocate_matrix(3, 3);
-    double b_arr[] = {8, 7, 6, 5, 4, 3, 2, 1, 0};
+    double b_arr[] = { 8, 7, 6, 5, 4, 3, 2, 1, 0 };
     generate_matrix(b, 3, 3, b_arr);
 
     int n, m;
@@ -68,15 +68,15 @@ void test3_sum(void)
 {
     double **right;
     right = allocate_matrix(3, 3);
-    double r_arr[] = {7, 7, 7, 7, 7, 7, 7, 7, 7};
+    double r_arr[] = { 7, 7, 7, 7, 7, 7, 7, 7, 7 };
     generate_matrix(right, 3, 3, r_arr);
 
     double **a = allocate_matrix(3, 3);
-    double a_arr[] = {6, 6, 6, 4, 4, 4, 2, 2, 2};
+    double a_arr[] = { 6, 6, 6, 4, 4, 4, 2, 2, 2 };
     generate_matrix(a, 3, 3, a_arr);
 
     double **b = allocate_matrix(3, 3);
-    double b_arr[] = {1, 1, 1, 3, 3, 3, 5, 5, 5};
+    double b_arr[] = { 1, 1, 1, 3, 3, 3, 5, 5, 5 };
     generate_matrix(b, 3, 3, b_arr);
 
     int n, m;
@@ -103,11 +103,11 @@ void test1_deg(void)
 {
     double **right;
     right = allocate_matrix(2, 2);
-    double r_arr[] = {7, 10, 15, 22};
+    double r_arr[] = { 7, 10, 15, 22 };
     generate_matrix(right, 2, 2, r_arr);
 
     double **a = allocate_matrix(2, 2);
-    double a_arr[] = {1, 2, 3, 4};
+    double a_arr[] = { 1, 2, 3, 4 };
     generate_matrix(a, 2, 2, a_arr);
 
     double **b = allocate_matrix(2, 2);
@@ -128,15 +128,15 @@ void test2_deg(void)
 {
     double **right;
     right = allocate_matrix(2, 2);
-    double r_arr[] = {50, 50, 50, 50};
+    double r_arr[] = { 50, 50, 50, 50 };
     generate_matrix(right, 2, 2, r_arr);
 
     double **a = allocate_matrix(2, 2);
-    double a_arr[] = {5, 5, 5, 5};
+    double a_arr[] = { 5, 5, 5, 5 };
     generate_matrix(a, 2, 2, a_arr);
 
     double **b = allocate_matrix(2, 2);
-    double b_arr[] = {5, 5, 5, 5};
+    double b_arr[] = { 5, 5, 5, 5 };
     generate_matrix(b, 2, 2, b_arr);
 
     int n, m;
@@ -154,15 +154,15 @@ void test3_deg(void)
 {
     double **right;
     right = allocate_matrix(2, 2);
-    double r_arr[] = {8, 5, 20, 13};
+    double r_arr[] = { 8, 5, 20, 13 };
     generate_matrix(right, 2, 2, r_arr);
 
     double **a = allocate_matrix(2, 2);
-    double a_arr[] = {1, 2, 3, 4};
+    double a_arr[] = { 1, 2, 3, 4 };
     generate_matrix(a, 2, 2, a_arr);
 
     double **b = allocate_matrix(2, 2);
-    double b_arr[] = {4, 3, 2, 1};
+    double b_arr[] = { 4, 3, 2, 1 };
     generate_matrix(b, 2, 2, b_arr);
 
     int n, m;
@@ -189,11 +189,11 @@ void test1_operation(void)
 {
     double **right;
     right = allocate_matrix(2, 2);
-    double r_arr[] = {-2, 1, 1.5, -0.5};
+    double r_arr[] = { -2, 1, 1.5, -0.5 };
     generate_matrix(right, 2, 2, r_arr);
 
     double **a = allocate_matrix(2, 2);
-    double a_arr[] = {1, 2, 3, 4};
+    double a_arr[] = { 1, 2, 3, 4 };
     generate_matrix(a, 2, 2, a_arr);
 
     int n, m;
@@ -210,11 +210,11 @@ void test2_operation(void)
 {
     double **right;
     right = allocate_matrix(2, 2);
-    double r_arr[] = {-0.875, 0.375, 0.625, -0.125};
+    double r_arr[] = { -0.875, 0.375, 0.625, -0.125 };
     generate_matrix(right, 2, 2, r_arr);
 
     double **a = allocate_matrix(2, 2);
-    double a_arr[] = {1, 3, 5, 7};
+    double a_arr[] = { 1, 3, 5, 7 };
     generate_matrix(a, 2, 2, a_arr);
 
     int n, m;
@@ -231,11 +231,11 @@ void test3_operation(void)
 {
     double **right;
     right = allocate_matrix(2, 2);
-    double r_arr[] = {1, -0.5, -2, 1.25};
+    double r_arr[] = { 1, -0.5, -2, 1.25 };
     generate_matrix(right, 2, 2, r_arr);
 
     double **a = allocate_matrix(2, 2);
-    double a_arr[] = {5, 2, 8, 4};
+    double a_arr[] = { 5, 2, 8, 4 };
     generate_matrix(a, 2, 2, a_arr);
 
     int n, m;
