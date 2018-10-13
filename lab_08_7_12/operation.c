d01991b093616416f55c9cb47f5d360b625b1606
Stepanov Alexandr <happymainer@gmail.com>
2018-10-13 09:09:43 +0300

    Fix compilation error in operation

--- lab_08_7_12/operation.c
+++ lab_08_7_12/operation.c
@@ -9,7 +9,7 @@ double **operation(double **matrix, int n, int m, int *nr, int *mr)
     double **ed = allocate_matrix(size, size);
 
     *nr = size;
-    *mr = size:
+    *mr = size;
 
     for (int i = 0; i < size; i++)
         for (int j = 0; j < size; j++)
