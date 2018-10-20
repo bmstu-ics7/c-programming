57657c7cdb942604dd7b439ac8f0cc6f9a4f6159
Stepanov Alexandr <happymainer@gmail.com>
2018-10-20 17:47:00 +0300

    Add tests

---
+++ lab_09_1/test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "assert.h"
+#include "my_strpbrk.h"
+
+void test1(void)
+{
+    char *a = "qwerty";
+    char *b = "er";
+
+    char *answ = my_strpbrk(a, b);
+    char *right = "erty";
+
+    assert_string(right, answ, "test1");
+}
+
+void test2(void)
+{
+    char *a = "qwerty";
+    char *b = "re";
+
+    char *answ = my_strpbrk(a, b);
+
+    assert_string(NULL, answ, "test2");
+}
+
+void test3(void)
+{
+    char *a = "kek";
+    char *b = "e";
+
+    char *answ = my_strpbrk(a, b);
+    char *right = "ek";
+
+    assert_string(right, answ, "test3");
+}
+
+void test(void)
+{
+    test1();
+    test2();
+    test3();
+
+    print_errors("test strpbrk");
+}
+
+int main(void)
+{
+    test();
+
+    return 0;
+}
