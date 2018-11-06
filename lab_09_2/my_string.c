09a6df2a51d4f17254274ae6a085becd82c817e0
Stepanov Alexandr <happymainer@gmail.com>
2018-11-06 11:39:01 +0300

    EDIT: test getline

--- lab_09_2/my_string.c
+++ lab_09_2/my_string.c
@@ -10,21 +10,31 @@ int len(char *string)
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
 {
     char buffer[255];
-    *n = 0;
 
-    if (lineptr == NULL || stream == NULL)
+    if (lineptr == NULL || stream == NULL || n == NULL)
         return GETLINE_ERROR;
+
+    if (*n == 0)
+        *n = 255;
+
+    ssize_t size = 0;
+
+    if (*lineptr == NULL)
+    {
+        *n = 255;
+        *lineptr = malloc(*n);
+    }
 
     while (fgets(buffer, 255, stream) != NULL)
     {
-        *n += len(buffer);
+        size += len(buffer);
         
-        *lineptr = realloc(*lineptr, *n);
+        *lineptr = realloc(*lineptr, size);
 
         if (*lineptr == NULL)
             return GETLINE_ERROR;
         
-        for (int i = *n - len(buffer), j = 0; i < *n; i++, j++)
+        for (int i = size - len(buffer), j = 0; i < size; i++, j++)
         {
             (*lineptr)[i] = buffer[j];
         }
@@ -39,12 +49,12 @@ ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
     if (len(*lineptr) == 0)
         return GETLINE_ERROR;
 
-    (*lineptr)[*n] = '\0';
+    (*lineptr)[size] = '\0';
 
     if (*n == 0)
         return GETLINE_ERROR;
 
-    return *n;
+    return size;
 }
 
 char *str_replace(const char *source, const char *search, const char *replace)
