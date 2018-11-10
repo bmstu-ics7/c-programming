d04430d82fa8cde22d29e3a2a67287453fd2f627
Stepanov Alexandr <happymainer@gmail.com>
2018-11-10 17:08:23 +0300

    ADD: main.c

---
+++ RK_31_2/main.c
@@ -0,0 +1,43 @@
+/*
+ * Ввод - 2
+ * Хранение - 4
+ * Задание - 2
+ * Вывод - 1
+ */
+
+#include <stdio.h>
+#include <errno.h>
+
+#include "file.h"
+#include "work.h"
+
+#define SUCCESS 0
+#define ARG_ERROR -2
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        return ARG_ERROR;
+
+    FILE *file = fopen(argv[1], "r");
+
+    int **matrix, n, m;
+
+    if (input_matrix(&matrix, &n, &m, file); != SUCCESS)
+    {
+        free_matrix(matrix);
+        return INPUT_ERROR;
+    }
+
+    fclose(file);
+
+    FILE *file = fopen("result.out", "w");
+
+    work(matrix, n, m, file);
+
+    fclose(file);
+    
+    free_matrix(matrix);
+
+    return SUCCESS;
+}
