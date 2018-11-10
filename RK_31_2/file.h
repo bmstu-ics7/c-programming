4e1608fbc448b64e89ecc76c3bfaef4bef9e8e54
U-VLAB\user-lab01 <user-lab01@LAB-01-70.vlab.bmstu.ru>
2018-11-10 17:05:16 +0300

    ADD: work with file

---
+++ RK_31_2/file.h
@@ -0,0 +1,19 @@
+#ifndef __FILE_H
+#define __FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 0
+#define INPUT_ERROR -1
+
+int input_matrix(int ***matrix, int *n, int *m, FILE *stream);
+
+void print_matrix(int **matrix, const int n, const int m, FILE *stream);
+
+void free_matrix(int **matrix, const int n);
+
+#endif
