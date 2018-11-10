fc7f517dec662be58ea1b521b1c823af520272c4
Stepanov Alexandr <happymainer@gmail.com>
2018-11-11 00:39:49 +0300

    ADD: functions for list

---
+++ lab_10/list.c
@@ -0,0 +1,93 @@
+#include "list.h"
+
+node_t *find(node_t *head, const void *data, int (*comparator)(const void*, const void *))
+{
+    node_t *temp = head;
+    
+    while (temp != NULL)
+    {
+        if (comparator(temp->data, data) == 0)
+            break;
+
+        temp = temp->next;
+    }
+
+    return temp;
+}
+
+void *pop_back(node_t **head)
+{
+    if (head == NULL)
+        return NULL;
+
+    if (*head == NULL)
+        return NULL;
+
+    node_t *temp = *head;
+    node_t *new_last = NULL;
+
+    while (temp->next != NULL)
+    {
+        new_last = temp;
+        temp = temp->next;
+    }
+
+    if (new_last == NULL)
+        *head = NULL;
+    else
+        new_last->next = NULL;
+
+    return temp->data;
+}
+
+node_t *sort(node_t *head, int (*comparator)(const void *, const void *))
+{
+    if (head == NULL)
+        return NULL;
+
+    if (comparator == NULL)
+        return NULL;
+
+    node_t *temp = head;
+    node_t *before_max = NULL;
+
+    node_t *check = head;
+
+    for (; check != NULL; check = check->next)
+    {
+        for (; temp->next != NULL; temp = temp->next)
+            if (comparator(before_max->next->data, temp->next->data) > 0)
+                before_max = temp;
+
+        node_t *max = before_max->next;
+        before_max->next = before_max->next->next;
+
+        sorted_insert(&head, max, comparator);
+    }
+
+    return head;
+}
+
+void sorted_insert(node_t **head, node_t *element, int (*comparator)(const void *, const void *))
+{
+    if (comparator(*head, element) > 0)
+    {
+        element->next = *head;
+        *head = element;
+        return;
+    }
+
+    node_t *temp = *head;
+
+    for (; temp->next != NULL; temp = temp->next)
+        if (comparator(element, temp) < 0)
+        {
+            element->next = temp->next;
+            temp->next = element;
+            return;
+        }
+
+    temp->next = element;
+    element->next = NULL;
+    return;
+}
---
+++ lab_10/list.h
@@ -0,0 +1,21 @@
+#ifndef __LIST_H
+#define __LIST_H
+
+#include <stdio.h>
+
+typedef struct node node_t;
+
+struct node {
+    void *data;
+    node_t *next;
+};
+
+node_t *find(node_t *head, const void *data, int (*comparator)(const void*, const void *));
+
+void* pop_back(node_t **head);
+
+node_t* sort(node_t *head, int (*comparator)(const void *, const void *));
+
+void sorted_insert(node_t **head, node_t *element, int (*comparator)(const void *, const void *));
+
+#endif
