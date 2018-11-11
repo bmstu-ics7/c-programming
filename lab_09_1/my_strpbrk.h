dfca72c87b6176e35640b9f42c09a7c20ed0b4e8
Stepanov Alexandr <happymainer@gmail.com>
2018-11-11 09:23:18 +0300

    DELETE: lab_09

--- lab_09_1/my_strpbrk.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#ifndef __MY_STRPBRK_H
-#define __MY_STRPBRK_H
-
-char *my_strpbrk(const char *string1, const char *string2);
-
-#endif
--- lab_09_2/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#include "my_string.h"
-
-int main(void)
-{
-    return 0;
-}
