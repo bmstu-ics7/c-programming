511889f0794bbfdb098e45642577ae92d171bd18
Stepanov Alexandr <happymainer@gmail.com>
2018-11-16 01:15:40 +0300

    FIX: return

--- lab_10/list.c
+++ lab_10/list.c
@@ -83,7 +83,7 @@ node_t *sort(node_t *head, int (*comparator)(const void *, const void *))
 void sorted_insert(node_t **head, node_t *element, int (*comparator)(const void *, const void *))
 {
     if (head == NULL || element == NULL || comparator == NULL || *head == NULL)
-        return NULL;
+        return;
 
     if (comparator((*head)->data, element->data) >= 0)
     {
