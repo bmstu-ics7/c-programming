95c7cc6c221e2f0594c97f07c3e682d6c94d5313
Stepanov Alexandr <happymainer@gmail.com>
2018-11-24 15:45:19 +0300

    EDIT: add cooments

--- lab_09_1/my_strpbrk.c
+++ lab_09_1/my_strpbrk.c
@@ -1,11 +1,28 @@
 #include "my_strpbrk.h"
 
+/*
+ * Находит длину строки
+ *
+ * @param string [in]
+ *
+ * @return длина строки
+ */
+
 int len(char *string)
 {
     int n = 0;
     while (string[n++] != '\0');
     return n - 1;
 }
+
+/*
+ * Находит первое вхождение символов из string2 в string1
+ *
+ * @param string1 [in]
+ * @param string2 [in]
+ *
+ * @return Указатель на первое вхождение или NULL в случае его отсутствия
+ */
 
 char *my_strpbrk(const char *string1, const char *string2)
 {
