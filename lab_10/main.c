1de09143ba89d548b1e12afb833654011622e778
Stepanov Alexandr <happymainer@gmail.com>
2018-11-30 19:48:24 +0300

    EDIT: clear main.c

--- lab_10/main.c
+++ lab_10/main.c
@@ -1,62 +1,8 @@
-#include "list.h"
+#include <stdio.h>
 
-int comparator(const void *data1, const void *data2)
-{
-    return *(int*)data1 - *(int*)data2;
-}
-
-void free_data(void *data)
-{
-    return;
-}
+#define SUCCESS 0
 
 int main(void)
 {
-    int arr[] = { 1, 3 ,5, 1, 3, 4, 2, 0, 3, 2, 3 };
-
-    node_t *head = malloc(sizeof(node_t));
-
-    node_t *temp = head;
-
-    for (int i = 0; i < 10; i++)
-    {
-        temp->data = arr + i;
-
-        if (i != 9)
-        {
-            temp->next = malloc(sizeof(node_t));
-            temp = temp->next;
-        }
-    }
-
-    temp->next = NULL;
-
-    temp = head;
-    while (temp != NULL)
-    {
-        printf("%d ", *(int*)temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-
-    head = sort(head, comparator);
-
-    temp = head;
-    while (temp != NULL)
-    {
-        printf("%d ", *(int*)temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-
-    remove_duplicates(&head, comparator);
-
-    temp = head;
-    while (temp != NULL)
-    {
-        printf("%d ", *(int*)temp->data);
-        temp = temp->next;
-    }
-
-    free_list(head, free_data);
+    return SUCCESS;
 }
