16d1cb33adc1ec35d1543328b63026f7a818cd16
Stepanov Alexandr <happymainer@gmail.com>
2018-12-13 15:17:11 +0300

    ADD: tests for my_snprintf

--- lab_11/test.c
+++ lab_11/test.c
@@ -1,22 +1,94 @@
 #include <stdio.h>
 
 #include "my_snprintf.h"
+#include "assert.h"
 
-int main(void)
+void test1(void)
 {
     char *str = malloc(100);
     char *my_str = malloc(100);
 
-    char a = '@';
-    int b = 0x12b;
-    int c = 123;
+    char a = '$';
+    int b = 0;
+    int c = 33;
 
-    printf("n: %d\n", snprintf(str, 100, "kek%c %x %o\n", a, b, c));
-    printf("my_n: %d\n", my_snprintf(my_str, 100, "kek%c %x %o\n", a, b, c));
+    assert(snprintf(str, 100, "asd %c %o %o %x %x", a, b, c, b, c), my_snprintf(my_str, 100, "asd %c %o %o %x %x", a, b, c, b, c), "test1 return value");
 
-    printf("str: %s\nmy_str: %s", str, my_str);
+    assert_string(str, my_str, "test1 string");
 
     free(str);
     free(my_str);
+}
+
+void test2(void)
+{
+    char *str = malloc(100);
+    char *my_str = malloc(100);
+
+    char a = '$';
+    int b = 0;
+    int c = 33;
+
+    assert(snprintf(str, 0, "asd %c %o %o %x %x", a, b, c, b, c), my_snprintf(my_str, 0, "asd %c %o %o %x %x", a, b, c, b, c), "test2 return value");
+
+    assert_string(str, my_str, "test2 string");
+
+    free(str);
+    free(my_str);
+}
+
+void test3(void)
+{
+    char *str = malloc(100);
+    char *my_str = malloc(100);
+
+    char a = '$';
+    short int b = 12;
+    short int c = 33;
+
+    assert(snprintf(str, 10, "%hx %ho", b, c), my_snprintf(my_str, 10, "%hx %ho", b, c), "test3 return value");
+
+    assert_string(str, my_str, "test3 string");
+
+    free(str);
+    free(my_str);
+}
+
+void test4(void)
+{
+    char *str = malloc(100);
+    char *my_str = malloc(100);
+
+    char a = '$';
+    int b = 1123;
+    int c = 33;
+
+    assert(snprintf(str, 100, "asd %c %o %o %x %x", a, b, c, b, c), my_snprintf(my_str, 100, "asd %c %o %o %x %x", a, b, c, b, c), "test4 return value");
+
+    assert_string(str, my_str, "test4 string");
+
+    free(str);
+    free(my_str);
+}
+
+int test_snprintf(void)
+{
+    int code_error = 0;
+
+    test1();
+    test2();
+    test3();
+    test4();
+
+    return print_errors("my_snprintf");
+}
+
+int main(void)
+{
+    if (test_snprintf() != SUCCESS)
+        return WRONG;
+
+    return SUCCESS;
+    
 }
 
